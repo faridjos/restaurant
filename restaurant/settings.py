43233911dc5afee10d039587dc7a8f391110b05a
faridjos <farid.benachenhou@hotmail.com>
2022-05-11 02:59:33 +0000

    Set DEBUG appropriately if in development or production

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -30,7 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+development = os.environ.get('DEVELOPMENT', False)
+DEBUG = development
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
