23eed1246ca313f5cb4a6d55a6d133ec05aee68a
faridjos <farid.benachenhou@hotmail.com>
2022-05-13 00:22:57 +0000

    Add data models of customers, restaurant tables, and bookings. Add code to display the fields of each model on the admin site

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Customer, Table, Booking
 
 # Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('fname', 'lname')
+    search_fields = ['lname']
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number_of_seats',)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'party_size', 'booking_time', 'time_of_booking')
+    list_filter = ('booking_time',)
+    search_fields = ['customer']
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="name", blank=True, null=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['lname']
+
+    def __str__(self):
+        return self.lname
+
+
+class Table(models.Model):
+    number_of_seats = models.IntegerField()
+
+    class Meta:
+        ordering = ['number_of_seats']
+
+
+class Booking(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="bookings")
+    tables = models.ManyToManyField(Table, related_name="bookings")
+    party_size = models.IntegerField()
+    booking_time = models.DateTimeField()
+    time_of_booking = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-booking_time']
